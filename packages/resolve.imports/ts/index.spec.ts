b178c64cee40c1daf1b910dc27eb8eefd6072d60
cyberuni <homawong@gmail.com>
2022-12-07 22:22:13 -0800

    fix: Support `subpath patterns` and `nested conditions`

--- packages/resolve.imports/ts/index.spec.ts
+++ packages/resolve.imports/ts/index.spec.ts
@@ -1,40 +1,125 @@
-import { expect, it } from '@jest/globals'
+import { describe, expect, it } from '@jest/globals'
 import { resolve } from './index.js'
 
-it('returns undefined when no imports field', () => {
-  const r = resolve({}, '#ansi-styles')
-  expect(r).toBeUndefined()
-})
+describe('no * pattern', () => {
+  it('returns undefined when no imports field', () => {
+    const r = resolve({}, '#ansi-styles')
+    expect(r).toBeUndefined()
+  })
 
-it('returns value if it is string', () => {
-  const r = resolve({
-    imports: {
-      '#ansi-styles': './browser.js',
-    },
-  }, '#ansi-styles')
-  expect(r).toBe('./browser.js')
-})
-
-it('returns first matched condition', () => {
-  const r = resolve({
-    imports: {
-      '#supports-color': {
-        node: './node.js',
-        default: './browser.js',
+  it('returns undefined when entry does not start with #', () => {
+    const r = resolve({
+      imports: {
+        '#ansi-styles': './browser.js',
       },
-    },
-  }, '#supports-color', { conditions: ['node', 'default'] })
-  expect(r).toBe('./node.js')
+    }, 'something-else')
+    expect(r).toBeUndefined()
+  })
+
+  it('returns value if it is string', () => {
+    const r = resolve({
+      imports: {
+        '#ansi-styles': './browser.js',
+      },
+    }, '#ansi-styles')
+    expect(r).toBe('./browser.js')
+  })
+
+  it('returns first matched condition', () => {
+    const r = resolve({
+      imports: {
+        '#supports-color': {
+          node: './node.js',
+          default: './browser.js',
+        },
+      },
+    }, '#supports-color', { conditions: ['node', 'default'] })
+    expect(r).toBe('./node.js')
+  })
+
+  it('accepts options without conditions', () => {
+    const r = resolve({
+      imports: {
+        '#supports-color': {
+          node: './node.js',
+          default: './browser.js',
+        },
+      },
+    }, '#supports-color', {})
+    expect(r).toBe('./browser.js')
+  })
+
+  it('works with nested conditions', () => {
+    const r = resolve({
+      imports: {
+        '#supports-color': {
+          node: {
+            import: './node.mjs',
+            require: './node.cjs',
+          },
+          default: './browser.mjs',
+        },
+      },
+    }, '#supports-color', { conditions: ['node', 'import'] })
+    expect(r).toBe('./node.mjs')
+  })
 })
 
-it('accepts options without conditions', () => {
-  const r = resolve({
-    imports: {
-      '#supports-color': {
-        node: './node.js',
-        default: './browser.js',
-      },
-    },
-  }, '#supports-color', {})
-  expect(r).toBe('./browser.js')
+describe(`subpath patterns`, () => {
+  it('maps to string pattern', () => {
+    const r = resolve({
+      imports: {
+        '#internal/*.js': './src/internal/*.js'
+      }
+    }, '#internal/foo.js')
+    expect(r).toBe('./src/internal/foo.js')
+  })
+
+  it('maps based on condition', () => {
+    const r = resolve({
+      imports: {
+        '#internal/*.js': {
+          node: './node/*.js',
+          default: './browser/*.js',
+        }
+      }
+    }, '#internal/foo.js', { conditions: ['node', 'default'] })
+    expect(r).toBe('./node/foo.js')
+  })
+
+  it('maps with nested conditions', () => {
+    const r = resolve({
+      "imports": {
+        '#internal/*.js': {
+          "node": {
+            "import": "./node/*.mjs",
+            "require": "./node/*.cjs"
+          },
+          "default": "./browser/*.mjs"
+        }
+      }
+    }, '#internal/foo.js', { conditions: ['node', 'import'] })
+    expect(r).toBe('./node/foo.mjs')
+  })
+
+  it('goes to the next map if first match failed condition checks', () => {
+    const r = resolve({
+      "imports": {
+        '#internal/*.js': {
+          "node": {
+            "require": "./node/*.cjs"
+          },
+          "default": "./browser/*.mjs"
+        },
+        '#internal/foo/*.js': {
+          "node": {
+            "import": "./foo/node/*.mjs"
+          },
+          "default": "./foo/browser/*.mjs"
+        }
+      }
+    }, '#internal/foo/bar.js', { conditions: ['node', 'import'] })
+    expect(r).toBe('./foo/node/bar.mjs')
+  })
 })
+
--- packages/resolve.imports/ts/index.ts
+++ packages/resolve.imports/ts/index.ts
@@ -1,15 +1,37 @@
 export function resolve(pkg: any, entry: string, options?: { conditions?: string[], extensions?: string[] }) {
   if (!pkg.imports) return undefined
+  if (!entry.startsWith('#')) return undefined
+
   const matched = pkg.imports[entry]
   if (matched) {
-    if (typeof matched === 'string') return matched
-    if (options?.conditions) {
-      for (const condition of options.conditions) {
-        if (matched[condition]) return matched[condition]
-      }
-    }
-    return matched.default
+    return lookupReplacer(pkg.imports[entry], options?.conditions)
   }
-  // TODO: support subpath patterns
+
+  for (const key in pkg.imports) {
+    const keyParts = key.split('*')
+    if (keyParts.length !== 2) continue
+
+    const [prefix, suffix] = keyParts
+    if (entry.startsWith(prefix)) {
+      const replacer = lookupReplacer(pkg.imports[key], options?.conditions?.slice())
+
+      if (!replacer) continue
+
+      const [rp, rs] = replacer.split('*')
+      return `${rp}${entry.slice(prefix.length, -suffix.length)}${rs}`
+    }
+  }
+
   return undefined
+}
+
+type ImportMap = string | { [key in string]: ImportMap }
+
+function lookupReplacer(map: ImportMap, conditions?: string[]): string | undefined {
+  if (typeof map === 'string') return map
+  if (conditions) {
+    const condition = conditions.shift()
+    return condition && map[condition] ? lookupReplacer(map[condition], conditions) : undefined
+  }
+  return map.default as any
 }
